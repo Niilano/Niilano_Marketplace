50da8355da3d937f5cfd03fb4c0756c7d7305d35
latif83 <latifm8360@gmail.com>
2024-01-01 02:06:03 +0000

    attempt change of app name and logo resize

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,16 +2,25 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.niilano.marketplace',
-  appName: 'NM',
+  appName: 'Niilano',
   webDir: 'dist/app/browser',
   bundledWebRuntime: false,
-  plugins : {
-    GoogleAuth : {
-      scopes : ["profile","email"],
-      serverClientId : "30578003989-fvauhmek8m2hp0gajaia6hqh3g9rohai.apps.googleusercontent.com",
-      forceCodeForRefreshToken : true
-    }
-  }
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId:
+        '30578003989-fvauhmek8m2hp0gajaia6hqh3g9rohai.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    },
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: '#de0f17',
+      showSpinner: false,
+      androidSpinnerStyle: 'small',
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
