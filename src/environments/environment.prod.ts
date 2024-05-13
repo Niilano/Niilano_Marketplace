5773a302f075ec2cb1ff689e8602005d6659f7d9
latif83 <latifm8360@gmail.com>
2024-05-13 10:41:54 +0000

    Changing backend host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // server:'https://niilano-marketplace-app.onrender.com',
   // wsServer : 'wss://niilano-marketplace-app.onrender.com',
-  server:'https://niilano-backend.cyclic.app',
+  server:'https://niilano-marketplace-app.vercel.app',
   wsServer : 'wss://niilano-backend.cyclic.app'
 };
